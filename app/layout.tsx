ea735cc82392e6f4a30ec35b4d3e10ab0cb04c89
sonenhur <sonen1125@gmail.com>
2024-08-12 16:00:15 +0900

    import 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import '@/app/ui/global.css';
-import {inter} from '@/app/ui/fonts';
-import React from "react";
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
                                        children,
