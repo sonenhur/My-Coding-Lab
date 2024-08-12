8e46d7e15abf29b25604e4c16b49b222dcfe400c
sonenhur <sonen1125@gmail.com>
2024-08-12 16:44:07 +0900

    chap.7 fetching data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,13 +6,13 @@ import { fetchRevenue, fetchLatestInvoices,fetchCardData } from '@/app/lib/data'
 
 export default async function Page() {
     const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
+    const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
     const {
         numberOfInvoices,
         numberOfCustomers,
         totalPaidInvoices,
         totalPendingInvoices,
-    } = await fetchCardData();
+    } = await fetchCardData(); // wait for fetchLatestInvoices() to finish
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
